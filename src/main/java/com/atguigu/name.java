0f4a714e3c35fdbe8295acaa0138d9d772f30239
lihuanyu <pluieisrain@163.com>
2020-03-29 16:16:34 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/name.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @Author Pluie
+ * @Date 2020/3/29 16:13
+ * @Version 1.0
+ */
+public class name {
+    public static void main(String[] args) {
+        System.out.println("riooooo");
+    }
+}
